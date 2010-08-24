bc495783e0335eaa85ab08ba995db32581d459a1
Steven Parkes <smparkes@smparkes.net>
2010-08-23 20:46:31 -0700

    reasonably working prototype

--- lib/zxing/objc/zxd.rb
+++ lib/zxing/objc/zxd.rb
@@ -47,55 +47,19 @@ class AppDelegate
     end
 
     capture.delegate = self
-
-    return
-
-    @session  =  QTCaptureSession.alloc.init
-
-    device = QTCaptureDevice.defaultInputDeviceWithMediaType QTMediaTypeVideo
-    device.open nil
-    input = QTCaptureDeviceInput.deviceInputWithDevice device
-    @session.addInput input, error:nil
-
-    output = QTCaptureDecompressedVideoOutput.alloc.init
-    output.automaticallyDropsLateVideoFrames = true
-    output.delegate = self
-
-    key = KCVPixelBufferPixelFormatTypeKey
-    value = NSNumber.numberWithUnsignedInt KCVPixelFormatType_32BGRA
-    pixelBufferAttributes = NSDictionary.dictionaryWithObject value, forKey:key
-    pixelBufferAttributes = {KCVPixelBufferPixelFormatTypeKey:KCVPixelFormatType_32BGRA}
-    output.pixelBufferAttributes = pixelBufferAttributes
-    @session.addOutput output, error:nil
-    
-    capture = QTCaptureLayer.alloc.initWithSession @session
-    capture.frame = @window.contentView.frame
-    mirror = CGAffineTransformMakeScale -1, 1
-    capture.affineTransform = mirror
-    main.addSublayer capture
-
-    frame = @window.contentView.frame
-    frame.origin.x = frame.size.width - frame.size.width*0.2;
-    frame.origin.y = 0;
-    frame.size.width = frame.size.width*0.2;
-    frame.size.height = frame.size.height*0.2;
-
-    @luminanceView = NSImageView.alloc.initWithFrame frame
-    @luminanceView.autoresizingMask = NSViewWidthSizable|NSViewHeightSizable
-    @window.contentView.addSubview @luminanceView
-
-    frame.origin.y += frame.size.height;
-    @binarizedView = NSImageView.alloc.initWithFrame frame
-
-    @binarizedView.autoresizingMask = NSViewWidthSizable|NSViewHeightSizable
-    @window.contentView.addSubview @binarizedView
-
-    @session.startRunning
   end
 
   def captureResult capture, result:result
-    p "capture result", capture, result
-    return !!@options[:continuous]
+    if @options[:continuous]
+      @count ||= 0
+      print "#{@count+=1} "
+    end
+    puts result.text
+    if !@options[:continuous]
+      capture.delegate = nil
+      capture.layer.removeFromSuperlayer
+      exit
+    end
   end
 
 end
